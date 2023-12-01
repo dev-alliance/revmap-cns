c7005744d0fa26e8887d96655629aa0e84db427b
dev-alliance <149306615+dev-alliance@users.noreply.github.com>
2023-12-01 17:32:48 +0500

    fix

--- src/service/api/axiosInstance.ts
+++ src/service/api/axiosInstance.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 //   baseURL: "http://localhost:1337",
 // });
 const baseURL = axios.create({
-  baseURL: "http://api.contractnsign.com", // Your API base URL
+  baseURL: "http://api.contractnsign.com", // Your API base URL11
 });
 
 export default baseURL;
