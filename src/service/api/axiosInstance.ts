0fbc2601e1450763c5509ec8c32407a715aff97a
shahbazasghar1038 <muhammadshahbaz1038@gmail.com>
2024-04-08 10:43:45 +0500

    aa

--- src/service/api/axiosInstance.ts
+++ src/service/api/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const baseURL = axios.create({
-  baseURL: "http://localhost:1337",
+// const baseURL = axios.create({
+//   baseURL: "http://localhost:1337",
 
-  // const baseURL = axios.create({
-  //   baseURL: "http://api.contractnsign.com",
+const baseURL = axios.create({
+  baseURL: "http://api.contractnsign.com",
 });
 
 export default baseURL;
