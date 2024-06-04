e76843050be6e4d53819c1cb5f7d68a4bb6440dc
shahbazasghar1038 <muhammadshahbaz1038@gmail.com>
2024-06-04 19:07:24 +0500

    test

--- src/service/api/axiosInstance.ts
+++ src/service/api/axiosInstance.ts
@@ -12,7 +12,7 @@ export default baseURL;
 // import axios from "axios";
 
 // const baseURL =
-//   process.env.NODE_ENV === "production"
+//   process.env.NODE_ENV === "productionsss"
 //     ? process.env.REACT_APP_API_BASE_URL_PROD
 //     : process.env.REACT_APP_API_BASE_URL_LOCAL;
 
